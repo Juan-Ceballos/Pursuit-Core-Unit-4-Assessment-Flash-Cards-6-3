d5222ccab5133c2c853ca470c55e744ff7e957b5
Juan C <juanceballos@pursuit.org>
2020-02-11 11:00:29 -0500

    setup windowscene and tab bar controller

---
+++ Unit4Assessment/Unit4Assessment/FlashcardTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  FlashcardTabBarController.swift
+//  Unit4Assessment
+//
+//  Created by Juan Ceballos on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class FlashcardTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemTeal
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
