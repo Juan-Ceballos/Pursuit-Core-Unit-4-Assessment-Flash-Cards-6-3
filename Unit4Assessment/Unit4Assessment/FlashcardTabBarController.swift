55c15841f0b345500306d087e8babbeb242c115d
Juan C <juanceballos@pursuit.org>
2020-02-11 11:26:18 -0500

    set up tab bar vc instances, and populated tab bar items

--- Unit4Assessment/Unit4Assessment/FlashcardTabBarController.swift
+++ Unit4Assessment/Unit4Assessment/FlashcardTabBarController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class FlashcardTabBarController: UITabBarController {
 
+    private lazy var flashcardCollectionVC: FlashcardCollection =  {
+        let viewController = FlashcardCollection()
+        viewController.tabBarItem = UITabBarItem(title: "cards", image: UIImage(systemName: "questionmark"), tag: 0)
+        return viewController
+    }()
+    
+    private lazy var createFlashcardCollectionVC: CreateFlashcardController = {
+        let viewControlelr = CreateFlashcardController()
+        viewControlelr.tabBarItem = UITabBarItem(title: "create", image: UIImage(systemName: "square.and.pencil"), tag: 1)
+        return viewControlelr
+    }()
+    
+    private lazy var searchFlaschcardVC: SearchFlashcardController = {
+        let viewController = SearchFlashcardController()
+        viewController.tabBarItem = UITabBarItem(title: "search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        return viewController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemTeal
-        viewControllers = [FlashcardCollection(), CreateFlashcardController(), SearchFlashcardController()]
+        viewControllers = [flashcardCollectionVC, createFlashcardCollectionVC, searchFlaschcardVC]
     }
     
 
